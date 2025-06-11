c6c04903374941114c02f1508af36d94d0b2f148
tdasguptaprediqt <tryambak.dasgupta@prediqt.it>
2025-06-11 10:59:23 +0530

    added search

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-const SearchSection = dynamic(() => import("@/components/SearchSection"));
-// const SearchSection = dynamic(() => import("@/components/ResultSection"));
+const SearchSection = dynamic(() => import("@/components/SearchSection")); // Search Component
+// const SearchSection = dynamic(() => import("@/components/ResultSection")); // Result Component
 
 function TestPage() {
   return (
