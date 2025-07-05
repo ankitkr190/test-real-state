6c381a32bf144a1b8faba985101a3bbd93085854
Arghadeep-hub <mallickarghadeep1@gmai.com>
2025-07-05 17:06:04 +0530

    new update

--- components/ChatSections/ChatFooterVoice.tsx
+++ components/ChatSections/ChatFooterVoice.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useRef } from "react";
 import { ChatMessage as ComponentsChatMessage } from "@livekit/components-react";
 import { useBrowserSpeech } from "../hooks/useBrowserSpeech";
