95e2ab85b4db516e262c0e56096b32bb2dced97a
tdaguptaprediqt <tryambak.dasgupta@prediqt.it>
2025-08-10 15:40:58 +0530

    added endpoint

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,9 @@ const nextConfig: NextConfig = {
     ENDPOINT_URL: "https://prod.apk.api.prediqt.ai", 
     // ENDPOINT_URL: "https://dev.api.harrods.rezolve.prediqt.ai",
     // ENDPOINT_URL: "http://127.0.0.1:8000",
+    NEXT_PUBLIC_LIVEKIT_URL: "wss://apk-ew2toxp5.livekit.cloud",
+    NEXT_PUBLIC_LIVEKIT_API_KEY: "APIBntaJVuLz4Dc",
+    NEXT_PUBLIC_LIVEKIT_API_SECRET: "FiFeh4P7Qq5GNegopTiGnGe4eHwEk6QcVY1eDMdgrguE",
   },
 };
 
