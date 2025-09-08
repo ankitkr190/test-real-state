248e6c078a20cc5068fcec4842fe897980d22c2b
tdaguptaprediqt <tryambak.dasgupta@prediqt.it>
2025-09-08 23:56:12 +0530

    added staging livekit

--- next.config.ts
+++ next.config.ts
@@ -17,9 +17,9 @@ const nextConfig: NextConfig = {
     TEST_ENDPOINT_URL: "https://test.apk.prediqt.it",
     // ENDPOINT_URL: "https://dev.api.harrods.rezolve.prediqt.ai",
     // ENDPOINT_URL: "http://127.0.0.1:8000",
-    NEXT_PUBLIC_LIVEKIT_URL: "wss://apk-ew2toxp5.livekit.cloud",
-    NEXT_PUBLIC_LIVEKIT_API_KEY: "APIBntaJVuLz4Dc",
-    NEXT_PUBLIC_LIVEKIT_API_SECRET: "FiFeh4P7Qq5GNegopTiGnGe4eHwEk6QcVY1eDMdgrguE",
+    NEXT_PUBLIC_LIVEKIT_URL: "wss://richytest-tftsuk11.livekit.cloud",
+    NEXT_PUBLIC_LIVEKIT_API_KEY: "APIPT95GnKVLMhf",
+    NEXT_PUBLIC_LIVEKIT_API_SECRET: "fk9ZwnGH9GA88l7F6nAUjLcsKzunrKSJuJVOlK0d3fI",
   },
 };
 
