002783b018fcbe21a32e34349b75c7dc12893d74
Arghadeep-hub <mallickarghadeep1@gmai.com>
2025-09-09 00:36:20 +0530

    removing the local

--- next.config.ts
+++ next.config.ts
@@ -17,9 +17,9 @@ const nextConfig: NextConfig = {
     TEST_ENDPOINT_URL: "https://test.apk.prediqt.it",
     // ENDPOINT_URL: "https://dev.api.harrods.rezolve.prediqt.ai",
     // ENDPOINT_URL: "http://127.0.0.1:8000",
-    NEXT_PUBLIC_LIVEKIT_URL: "wss://richytest-tftsuk11.livekit.cloud",
-    NEXT_PUBLIC_LIVEKIT_API_KEY: "APIPT95GnKVLMhf",
-    NEXT_PUBLIC_LIVEKIT_API_SECRET: "fk9ZwnGH9GA88l7F6nAUjLcsKzunrKSJuJVOlK0d3fI",
+    // NEXT_PUBLIC_LIVEKIT_URL: "wss://richytest-tftsuk11.livekit.cloud",
+    // NEXT_PUBLIC_LIVEKIT_API_KEY: "APIPT95GnKVLMhf",
+    // NEXT_PUBLIC_LIVEKIT_API_SECRET: "fk9ZwnGH9GA88l7F6nAUjLcsKzunrKSJuJVOlK0d3fI",
   },
 };
 
